919894da4c3cfe52c344a195de5d7e943a60f0ea
Kunal Chugh <kchugh2@illinois.edu>
2015-10-06 15:03:37 -0400

    second commit

---
+++ InvoiceProj/src/mypackage/invoiceApp2.java
@@ -0,0 +1,73 @@
+package mypackage;
+import java.util.*;
+import mypackage.lineOrder2;
+
+public class invoiceApp2 {
+
+	public static void main(String[] args)
+	{
+		Date d = new Date();
+		Scanner keyboard = new Scanner(System.in);
+		String book_author, book_title, book_description,yesno,done="n",customer_name;
+		int quantinstock, i=0,invoicenumb;
+		int noofcopies[] = new int[50];
+		double pricepercopy, totalprice, sum=0,tax=0,taxsum=0;
+		boolean istaxable;
+		lineOrder2 b[] = new lineOrder2[50];
+		System.out.println("Enter the Invoice number");
+		invoicenumb = keyboard.nextInt();
+		System.out.println("Enter customer Name");
+		keyboard.nextLine();
+		customer_name=keyboard.nextLine();
+		keyboard.nextLine();
+		
+		do
+		{
+			System.out.println("Item Description");
+			book_author = keyboard.nextLine();
+			/*System.out.println("Book Title");
+			book_title = keyboard.nextLine();
+			System.out.println("Genre");
+			book_description = keyboard.nextLine();*/
+			System.out.println("Is this purchase taxable ? [yes/no]");
+			yesno=keyboard.nextLine();
+			keyboard.nextLine();
+				if(yesno.equals("yes"))
+					istaxable =true;
+				else 
+					istaxable = false;
+		
+			System.out.println("quantity?");
+			noofcopies[i] = keyboard.nextInt();
+			quantinstock = 5000;
+			System.out.println("Price ?\n");
+			pricepercopy=keyboard.nextDouble();
+			b[i]= new lineOrder2(book_author,quantinstock,pricepercopy,invoicenumb,istaxable);
+			/*System.out.println("How many copies do you want to buy?");
+			noofcopies[i] = keyboard.nextInt();*/
+			totalprice = b[i].getTotalPrice(noofcopies[i]);
+			tax=b[i].getTax(noofcopies[i]);
+			sum= sum+totalprice;
+			taxsum=taxsum+tax;
+			i++;
+			keyboard.nextLine();
+			System.out.println("are you done ? [y/n]");
+			done=keyboard.nextLine();
+			
+			}	while(done.equals("n"));
+		
+		
+		System.out.println("\nInvoice number\t"+invoicenumb);
+		System.out.println("\nCustomer Name\t"+ customer_name + "\tDate:\t"+d );
+		System.out.println("Author\tPrice\tQty\tTotal\tTax\n");
+		for(int j = 0; j< i ; j++)
+		{
+			System.out.printf(b[j].getAuthor()+"\t"+/*b[j].getTitle()+"\t\t"+b[j].getDescription()+*/+b[j].getPrice()+"\t"+noofcopies[j]+"\t"+b[j].getTotalPrice(noofcopies[j])+"\t\t"+b[j].getTax(noofcopies[j]));
+			
+		}
+		System.out.println("\nSubTotal:\t"+ sum + "\nTax      :\t"+taxsum+"\nTotal Amount:\t"+ (sum+taxsum));
+		
+		
+	}
+
+}
---
+++ InvoiceProj/src/mypackage/lineOrder2.java
@@ -0,0 +1,106 @@
+package mypackage;
+
+public class lineOrder2 {
+
+	private String author, title, description;
+	private int quantity,invoice_number;
+	private double price;
+	private boolean isInStock,taxable; 
+	/*variable declaration*/
+	public lineOrder2( String aut,int stk, double cost, int invoiceno, boolean istaxable)
+	{
+	setAuthor(aut);
+	/*setTitle(tit);
+	setDescription(descript);*/
+	invoice_number=invoiceno;
+	quantity =stk;
+	price =cost;
+	taxable=istaxable;
+	
+	if (stk > 0)
+		isInStock = true;
+	else 
+		isInStock = false;
+	}
+	public lineOrder2()
+	{
+	setAuthor("");
+	/*setTitle("");
+	setDescription("");*/
+	taxable = false;
+	quantity = 0;
+	price = 0;
+	isInStock = false;
+	invoice_number = 0 ;
+	}
+	
+	
+	public double getTotalPrice (int reqQuantity)
+	{
+		double subtotal=0;
+		
+			if(this.isInStock == true)
+			{ 
+				if(reqQuantity<=this.quantity)
+				subtotal = reqQuantity * this.price;
+				else
+				subtotal = 0;
+			}
+				
+		
+			else
+				subtotal = 0;	
+	
+			
+		return subtotal;
+	}
+	
+
+	public double getTax (int reqQuantity)
+	{
+		double subtotal=0,tax =0, taxrate = 1;
+		if(this.isInStock)
+		{	
+			if(this.taxable==true&&reqQuantity<=this.quantity)
+				{
+				subtotal = reqQuantity * this.price;
+				tax=taxrate*subtotal;
+				
+				}
+			else
+				tax =0;
+				}
+		
+		return tax;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(int p) {
+		this.price=p;
+	}	
+	
+	
+		
+	}
